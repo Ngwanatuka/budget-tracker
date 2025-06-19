8d158382db2bb5f6ec6b2f463f029ec450dde173
ngmolepjhb024 <ngmolepjhb024@student.wethinkcode.co.za>
2025-06-19 20:00:33 +0200

    create_app() to take arguments

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
     app.secret_key = 'secret'
+
+    if test_config:
+        app.config.update(test_config)
 
     from . import routes
     app.register_blueprint(routes.bp)
