7e26c1ee49fb8fb253fd9784f23cf8a26843b0cf
ngmolepjhb024 <ngmolepjhb024@student.wethinkcode.co.za>
2025-06-20 23:07:20 +0200

    Fix run.py for Railway deployment

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 from app import create_app
-from app.models import db
 import os
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+app = create_app()
 
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
