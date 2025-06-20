9e91af816776222e38160f33178721461d7d3995
ngmolepjhb024 <ngmolepjhb024@student.wethinkcode.co.za>
2025-06-20 23:06:07 +0200

    Add gunicorn to requirements and Procfile

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import create_app
 from app.models import db
-
-app = create_app()
+import os
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
