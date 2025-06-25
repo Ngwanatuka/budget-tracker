b78be9c373ce5291bc203ff990d327974d4f5b17
Ngwanatuka <ngmolepjhb024@student.wethinkcode.co.za>
2025-06-25 10:26:22 +0200

    Refactor imports and add CSRF token to login and register templates

--- app/auth.py
+++ app/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models.user import User
-from app.models import db
-import datetime
+from app import db
+from datetime import datetime
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
